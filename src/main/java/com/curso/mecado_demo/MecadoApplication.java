694790d8e8ab47412a2d68a60c53a27e8df7187b
Octavio <octa8000@gmail.com>
2024-11-20 21:42:22 -0600

    Agregando recursos iniciales

---
+++ src/main/java/com/curso/mecado_demo/MecadoApplication.java
@@ -0,0 +1,13 @@
+package com.curso.mecado_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MecadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MecadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/mecado_demo/MecadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.mecado_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MecadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
